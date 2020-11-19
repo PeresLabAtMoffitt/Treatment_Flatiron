17456ae748f68c8a74f72a3b5d3e3463c22b8980
chrisleitzinger <christelleleitzinger@gmail.com>
2020-11-19 17:03:32 -0500

    Update cleaning

--- R/2.data cleaning.R
+++ R/2.data cleaning.R
@@ -5,29 +5,51 @@ creatinine <- creatinine %>%
     testresultcleaned > 0.8     ~ 0.8,
     TRUE                       ~ testresultcleaned
   )) %>% 
+  mutate(testdate = as.Date(testdate, format = "%m/%d/%y")) %>% 
   select(c("patientid", cr_date= "testdate", "creatinine", unit_creat = "testunitscleaned"))
 
 body_surface_area <- vitals %>% 
   filter(labcomponent == "Body Surface Area (BSA)") %>% 
+  mutate(testdate = as.Date(testdate, format = "%m/%d/%y")) %>% 
   select(c("patientid", BSA_date = "testdate", BSA = "testresult")) %>% 
   mutate(BSA_units = "m2")
 
+vitals <- vitals %>% 
+  mutate(testdate = as.Date(testdate, format = "%m/%d/%y"))
+
 height <- vitals %>% 
   filter(test == "body height") %>% 
-  select(c("patientid", "testdate", "testunits", "testresult")) %>% 
   mutate(height = case_when(
-    testunits == "cm"     ~ testresult/2.54,
-    testunits == "ft"     ~ testresult*12,
-    testunits == "ft"     ~ testresult*12
+    !is.na(testresultcleaned)               ~ round(testresultcleaned/100, 3),
+    testresult %in% c(1:7)                  ~ round(testresult/3.281, 3),
+    !is.na(testresult)                      ~ round(testresult/39.37, 3)
     )) %>% 
-  mutate(year = year(testdate)) %>% 
-  arrange(testdate) %>% 
-  distinct(patientid, year, .keep_all = TRUE)
+  select(c("patientid", height_date = "testdate", "height")) %>% 
+  mutate(height_units = "m") %>% 
+  mutate(year = year(height_date))# %>%
+  # arrange(height_date)# %>%
+  # distinct(patientid, year, .keep_all = TRUE)
 
+weight <- vitals %>% 
+  filter(test == "body weight") %>% 
+  select(c("patientid", weight = "testresultcleaned")) %>% 
+  mutate(weight_units = "kg")# %>% 
+  
+  # mutate(year = year(testdate)) %>% 
+  # arrange(testdate) %>% 
+  # distinct(patientid, year, .keep_all = TRUE) 
 
+weight1 <- vitals %>% 
+  filter(test == "body weight") %>% 
+  select(c("patientid", "testdate", "testresult")) %>% 
+  filter(is.na(testresult))
+uid <- paste(unique(weight1$patientid), collapse = '|')
 
-
-
+weight2 <- vitals %>% 
+  filter(test == "body weight") %>% 
+  select(c("patientid", "testdate", "testresult")) %>% 
+  filter(!is.na(testresult))
+weight3 <- weight2[(grepl(uid, weight2$patientid)),]
 
 
 
@@ -35,21 +57,9 @@ height <- vitals %>%
 ############ BMI
 
 
-Vitals <- Vitals %>% 
-  filter(test == "body weight") %>% 
-  select(c("patientid", "testdate", "testunits", "testresult")) %>% 
-  mutate(weight = case_when(
-    testunits == "kg"     ~ testresult*2.205,
-    testunits == "oz"     ~ testresult/16,
-    TRUE                  ~ testresult
-  )) %>% 
-  mutate(year = year(testdate)) %>% 
-  arrange(testdate) %>% 
-  distinct(patientid, year, .keep_all = TRUE) %>% 
+%>% 
   full_join(., Vitals_height, by = c("patientid", "year"))
 mutate(BMI = )
 
 # Vital <- dcast(setDT(Vitals), patientid+testdate ~ rowid(patientid),  value.var = c("test", "testunits", "testresult"))
 
-
-table(Vitals_height$testunits)
