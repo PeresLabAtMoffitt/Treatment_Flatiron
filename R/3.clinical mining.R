42cf3b465e38e26042e7368bba0f6361d9c43851
chrisleitzinger <christelleleitzinger@gmail.com>
2021-01-14 17:30:21 -0500

    ggplot

--- R/3.clinical mining.R
+++ R/3.clinical mining.R
@@ -1,5 +1,35 @@
 #################################################################################################### I ### Treatment Mining
-summ <- therapy %>% group_by(patientid, linename, chemotherapy_type) %>% 
+# 1.	Determining frontline treatment regimen
+
+Treatment %>% distinct(patientid, .keep_all = TRUE) %>% 
+  ggplot(aes(x= therapy))+
+  geom_bar()+
+  theme_minimal()+
+  labs(title = "Frontline treatment")+
+  coord_flip()
+
+Treatment %>% distinct(patientid, .keep_all = TRUE) %>% 
+  ggplot(aes(x= chemotherapy_type))+
+  geom_bar()+
+  theme_minimal()+
+  labs(title = "Chemotherapy type")+
+  coord_flip()
+
+Treatment %>% distinct(patientid, .keep_all = TRUE) %>% 
+  ggplot(aes(x= chemo))+
+  geom_bar()+
+  theme_minimal()+
+  coord_flip()
+
+
+
+
+
+
+
+
+#
+summ <- therapy1 %>% group_by(patientid, linename, chemotherapy_type) %>% 
   count(linename, chemotherapy_type, drugname, name = "drug_repeat_per_line") %>% 
   group_by(patientid, linename, chemotherapy_type) %>% 
   summarise_at(vars(drugname, drug_repeat_per_line), paste, collapse = "-")
