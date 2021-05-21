ccca4eda97e89afdb415f579e598d2c19d170010
chrisleitzinger <christelleleitzinger@gmail.com>
2021-05-21 16:38:54 -0400

    Add second map

--- R/maps.R
+++ R/maps.R
@@ -1,5 +1,5 @@
 # Map
-
+library(tidyverse)
 library(scatterpie)
 
 Frontline <- read_rds("/Users/colinccm/Documents/GitHub/Peres/Treatment_Flatiron/Frontline.rds")
@@ -11,7 +11,7 @@ state_coor <- read_csv("/Users/colinccm/Documents/GitHub/Peres/data/statelatlong
 state_coor <- state_coor %>% 
   group_by(State, practiceid) %>% 
   mutate(nbr_patient_by_practice_id = n()) %>% 
-  mutate(attendance = ifelse(nbr_patient_by_practice_id > 150, "popular", "countryside")) %>% 
+  mutate(attendance = ifelse(nbr_patient_by_practice_id > 150, "high attendance", "low attendance")) %>% 
   select(State, nbr_patient_by_practice_id, attendance, everything())
 
 state_coor1 <- state_coor %>% 
@@ -56,3 +56,98 @@ ggplot(us, aes(long, lat)) +
         axis.title = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank())
+
+
+# RDI by attendance
+
+state_coor1 <- state_coor %>% 
+  mutate(p = 1) %>% # create p == 1 to make addition each row
+  filter(RDI_grp == "RDI < 0.85") %>% 
+  group_by(State, Longitude, Latitude, RDI_grp, attendance) %>% 
+  select(State, Longitude, Latitude, RDI_grp, p, attendance) %>% 
+  summarize(across(c("p"), ~ sum(.x, na.rm = TRUE))) %>% 
+  group_by(State, Longitude, Latitude) %>% 
+  pivot_wider(names_from = "attendance", values_from = "p") %>% 
+  ungroup()
+
+state_coor2 <- state_coor1 %>% 
+  mutate(total = rowSums( select(., "low attendance":"high attendance") , na.rm = TRUE))
+# rowwise() %>%
+# mutate(sum = sum(select(.,NHWhite:Other), na.rm = T))
+
+
+ggplot(us, aes(long, lat)) +
+  geom_map(map=us, aes(map_id=region), fill="grey97", color="grey") +
+  geom_scatterpie(data = state_coor2, 
+                  aes(Longitude, Latitude, r = .7),
+                  # Try to calculate ration with state size
+                  # or ratio with total pop sqrt(total_pop)/100 
+                  cols = c("low attendance","high attendance"), 
+                  alpha = 0.5) +
+  # scale_fill_manual(
+  #   breaks = c("RDI >= 0.85","RDI < 0.85"),
+  #   labels = c("right dose or higher","underdose"),
+  #   values = c("RDI >= 0.85" = "lightgreen",
+  #              "RDI < 0.85" = "red")
+  # ) +
+  labs(title = "Flatiron",
+       # subtitle = "RDI",
+       caption = "Source: Flatiron",
+       fill = NULL) +
+  coord_fixed() +
+  theme_bw() +
+  theme(#legend.position = c(1, 0.02),
+    legend.justification = c(1, 0),
+    panel.grid = element_blank(),
+    # panel.border = element_blank(),
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank())
+
+
+#
+state_coor1 <- state_coor %>% 
+  mutate(p = 1) %>% # create p == 1 to make addition each row
+  # filter(RDI_grp == "RDI < 0.85") %>% 
+  group_by(State, Longitude, Latitude, RDI_grp, attendance) %>% 
+  select(State, Longitude, Latitude, RDI_grp, p, attendance) %>% 
+  summarize(across(c("p"), ~ sum(.x, na.rm = TRUE))) %>% 
+  group_by(State, Longitude, Latitude) %>% 
+  pivot_wider(names_from = c("attendance", "RDI_grp"), values_from = "p") %>% 
+  ungroup()
+
+state_coor2 <- state_coor1 %>% 
+  mutate(total = rowSums( select(., "low attendance_RDI >= 0.85", "high attendance_RDI >= 0.85", "low attendance_RDI < 0.85", "high attendance_RDI < 0.85") , na.rm = TRUE))
+# rowwise() %>%
+# mutate(sum = sum(select(.,NHWhite:Other), na.rm = T))
+
+
+ggplot(us, aes(long, lat)) +
+  geom_map(map=us, aes(map_id=region), fill="grey97", color="grey") +
+  geom_scatterpie(data = state_coor2, 
+                  aes(Longitude, Latitude, r = .7),
+                  # Try to calculate ration with state size
+                  # or ratio with total pop sqrt(total_pop)/100 
+                  cols = c("low attendance_RDI >= 0.85", "high attendance_RDI >= 0.85", "low attendance_RDI < 0.85", "high attendance_RDI < 0.85"), 
+                  alpha = 0.5) +
+  scale_fill_manual(
+    # breaks = c("RDI >= 0.85","RDI < 0.85"),
+    # labels = c("right dose or higher","underdose"),
+    values = c("low attendance_RDI >= 0.85" = "lightgreen",
+               "high attendance_RDI >= 0.85" = "darkgreen",
+               "low attendance_RDI < 0.85" = "red",
+               "high attendance_RDI < 0.85" = "darkred")
+  ) +
+  labs(title = "Flatiron",
+       # subtitle = "RDI",
+       caption = "Source: Flatiron",
+       fill = NULL) +
+  coord_fixed() +
+  theme_bw() +
+  theme(#legend.position = c(1, 0.02),
+    legend.justification = c(1, 0),
+    panel.grid = element_blank(),
+    # panel.border = element_blank(),
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank())
