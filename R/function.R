b531fff78be6d30e81d3f87f6402a286a21f9acc
chrisleitzinger <christelleleitzinger@gmail.com>
2021-12-23 15:58:31 -0500

    Update drake

--- R/function.R
+++ R/function.R
@@ -8,31 +8,31 @@ path <- fs::path("","Volumes","Peres_Research")
 # }
 data_import <- function(path){
   read_csv(paste0(path,
-                  "/Flatiron/Data from Flatiron/ovarian/NEW_ovarianCLEANwithbiom.csv"))
+                  "/Flatiron/Data from Flatiron/edm_ovarian_472021/NEW_ovarianCLEANwithbiom.csv"))
 }
 vitals_import <- function(path){
   read_csv(paste0(path,
-                  "/Flatiron/Data from Flatiron/ovarian/NEW_vitals_anthro.csv"))
+                  "/Flatiron/Data from Flatiron/edm_ovarian_472021/NEW_vitals_anthro.csv"))
 }
 
 cr_import <- function(path){
   read_csv(paste0(path,
-                  "/Flatiron/Data from Flatiron/ovarian/NEW_labs_creatinine.csv"))
+                  "/Flatiron/Data from Flatiron/edm_ovarian_472021/NEW_labs_creatinine.csv"))
 }
 
 auc_import <- function(path){
   read_csv(paste0(path,
-                  "/Flatiron/Data from Flatiron/ovarian/ovarian_medicationorder.csv"))
+                  "/Flatiron/Data from Flatiron/edm_ovarian_472021/MedicationOrder.csv"))
 }
 
 drugs_import <- function(path){
   read_csv(paste0(path,
-                  "/Flatiron/Data from Flatiron/ovarian/ovarian_drugepisode.csv"))
+                  "/Flatiron/Data from Flatiron/edm_ovarian_472021/DrugEpisode.csv"))
 }
 
 orderdrug_import <- function(path){
   read_csv(paste0(path,
-                  "/Flatiron/Data from Flatiron/ovarian/ovarian_medicationadministration.csv"))
+                  "/Flatiron/Data from Flatiron/edm_ovarian_472021/MedicationAdministration.csv"))
 }
 
 
